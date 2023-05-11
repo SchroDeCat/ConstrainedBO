bug fix, including regret calculation

src/opt/constrained.py
@@ -18,7 +18,7 @@ import datetime
 import itertools
 
 from ..models import DKL, AE, beta_CI
-from ..utils import save_res, load_res, clustering_methods, model_list_CI, intersecting_ROI_globe
+from ..utils import save_res, load_res, clustering_methods, model_list_CI, intersecting_ROI_globe, feasible_filter_gen
 from .dkbo_olp import DK_BO_OLP
 from .dkbo_ae_constrained import DK_BO_AE_C, DK_BO_AE_C_M
 from math import ceil, floor
@@ -57,7 +57,8 @@ def cbo(x_tensor, y_tensor, c_tensor, constraint_threshold, constraint_confidenc
     feasibility_filter = c_tensor > c_threshold
     assert sum(feasibility_filter) > 0
     name = name if low_dim else name+'-hd'
-    max_val = y_tensor.max()
+    feasible_filter = feasible_filter_gen([c_tensor], [c_threshold])
+    max_val = y_tensor[feasible_filter].max()
     reg_record = np.zeros([n_repeat, n_iter])
     ratio_record = np.zeros([n_repeat, n_iter])
     max_LUCB_interval_record = np.zeros([n_repeat, 3, n_iter]) # 0 - Global, 1 - ROI, 2 -- intersection
@@ -354,7 +355,9 @@ def cbo_multi(x_tensor, y_tensor, c_tensor_list, constraint_threshold_list, cons
     
     assert sum(feasibility_filter) > 0
     name = name if low_dim else name+'-hd'
-    max_val = y_tensor.max()
+
+    feasible_filter = feasible_filter_gen(c_tensor_list, c_threshold_list)
+    max_val = y_tensor[feasible_filter].max()
     reg_record = np.zeros([n_repeat, n_iter])
     ratio_record = np.zeros([n_repeat, n_iter])
     max_LUCB_interval_record = np.zeros([n_repeat, 3, n_iter]) # 0 - Global, 1 - ROI, 2 -- intersection
@@ -497,7 +500,7 @@ def cbo_multi(x_tensor, y_tensor, c_tensor_list, constraint_threshold_list, cons
                 else:
                     _f_model_passed_in, _c_model_list_passed_in = _f_model, _c_model_list
                 _cbo_m = DK_BO_AE_C_M(x_tensor, y_tensor, c_tensor_list, roi_filter, c_uci_filter_list, lr=lr, spectrum_norm=spectrum_norm, low_dim=low_dim,
-                                    n_init=n_init,  train_iter=train_times, regularize=regularize, dynamic_weight=False,  retrain_nn=True, c_threshold=c_threshold_list,
+                                    n_init=n_init,  train_iter=train_times, regularize=regularize, dynamic_weight=False,  retrain_nn=True, c_threshold_list=c_threshold_list,
                                     max=max_val, pretrained_nn=ae, verbose=verbose, init_x=init_x, init_y=init_y, init_c_list=init_c_list, exact_gp=exact_gp, noise_constraint=roi_noise_constraint,
                                     f_model=_f_model_passed_in, c_model_list=_c_model_list_passed_in)
 

src/opt/dkbo_ae_constrained.py
@@ -28,6 +28,8 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.neighbors import NearestNeighbors
 
+from ..utils import feasible_filter_gen
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DK_BO_AE_C():
@@ -58,7 +60,8 @@ class DK_BO_AE_C():
         self.data_size = self.x_tensor.size(0)
         self.train_iter = train_iter
         self.retrain_nn = retrain_nn
-        self.maximum = torch.max(self.y_tensor) if max==None else max
+        feasible_filter = feasible_filter_gen([c_tensor], [c_threshold])
+        self.maximum = torch.max(self.y_tenso[feasible_filter]) if max==None else max
         self.max_regret = self.maximum - torch.min(self.y_tensor)
 
         self.init_x = kwargs.get("init_x", self.x_tensor[:n_init])        
@@ -316,17 +319,19 @@ class DK_BO_AE_C_M():
         self.dynamic_weight = dynamic_weight
         self.x_tensor = torch.from_numpy(x_tensor).float()
         self.y_tensor = y_tensor.float()
-        self.c_tensor_list = c_tensor_list.float()
+        self.c_tensor_list = [c_tensor.float() for c_tensor in c_tensor_list]
         self.data_size = self.x_tensor.size(0)
         self.train_iter = train_iter
         self.retrain_nn = retrain_nn
-        self.maximum = torch.max(self.y_tensor) if max==None else max
+        feasible_filter = feasible_filter_gen(c_tensor_list, c_threshold_list)
+        self.maximum = torch.max(self.y_tensor[feasible_filter]) if max==None else max
         self.max_regret = self.maximum - torch.min(self.y_tensor)
 
         self.init_x = kwargs.get("init_x", self.x_tensor[:n_init])        
         self.init_y = kwargs.get("init_y", self.y_tensor[:n_init])
         self.init_c_list = kwargs.get("init_c_list", [c_tensor[:n_init] for c_tensor in self.c_tensor_list])
-        assert self.init_x.size(0) == self.init_c.size(0)
+        for c_idx in range(self.c_num):
+            assert self.init_x.size(0) == self.init_c_list[c_idx].size(0)
         if "init_x" in kwargs:
             self.init_x = torch.from_numpy(self.scaler.transform(self.init_x)).float()
         self.spectrum_norm = spectrum_norm
@@ -390,7 +395,7 @@ class DK_BO_AE_C_M():
         '''
         First Stage: Query both f and c simultaneously
         '''
-        assert self.init_x.size(0) == self.init_c.size(0)
+        assert self.init_x.size(0) == self.init_c_list[0].size(0)
         assert self.init_x.size(0) == self.init_y.size(0)
         self.regret = np.zeros(n_iter)
         if_tqdm = kwargs.get("if_tqdm", False)

test_cbo_m.py
@@ -21,7 +21,7 @@ from botorch.models.model_list_gp_regression import ModelListGP
 from botorch.test_functions import Ackley
 from botorch.utils.transforms import unnormalize
 
-from src.opt import cbo
+from src.opt import cbo, cbo_multi
 from src.utils import sample_pts, feasible_filter_gen
 # from .scbo_botorch import SCBO
 
@@ -39,33 +39,41 @@ fun.bounds[0, :].fill_(-5)
 fun.bounds[1, :].fill_(10)
 dim = fun.dim
 lb, ub = fun.bounds
-n_pts = 20000
+n_pts = 10000
 
-def c_fun(x):  # Equivalent to enforcing that x[0] >= 0
+def c_fun_1(x):  # Equivalent to enforcing that x[0] >= 0
     return x[0]
 
+def c_fun_2(x):  # Equivalent to enforcing that x[0] >= 0
+    return x[1]
+
+c_fun_list = [c_fun_1, c_fun_2]
+c_num = len(c_fun_list)
+
 n_init = 10
-n_iter = 100
+n_iter = 40
 train_times = 5
 n_repeat = 2
 max_cholesky_size = float("inf")  # Always use Cholesky
 
 
-
 x_tensor = sample_pts(lb, ub, n_pts, dim=dim).to(dtype=dtype, device=device)
 y_tensor = torch.tensor([fun(x) for x in x_tensor], dtype=dtype, device=device).unsqueeze(-1)
-c_tensor = torch.tensor([c_fun(x) for x in x_tensor], dtype=dtype, device=device).unsqueeze(-1)
-feasible_filter = feasible_filter_gen([c_tensor], [0])
-print(f"initial reward {y_tensor[:n_init][feasible_filter[:n_init]].squeeze()} while global max {y_tensor[feasible_filter].max().item()}")
+c_tensor_list = [torch.tensor([c_fun_list[c_idx](x) for x in x_tensor], dtype=dtype, device=device).unsqueeze(-1) for c_idx in range(c_num)]
+constraint_threshold_list = torch.zeros(c_num)
+constraint_confidence_list = torch.ones(c_num) * 0.5
+feasible_filter = feasible_filter_gen(c_tensor_list, constraint_threshold_list)
 
-regret = cbo(x_tensor, y_tensor, c_tensor, constraint_threshold=0, constraint_confidence=.5,
+print(f"initial reward {y_tensor[:n_init][feasible_filter[:n_init]]} while global max {y_tensor[feasible_filter].max().item()}")
+
+regret = cbo_multi(x_tensor, y_tensor, c_tensor_list, constraint_threshold_list=constraint_threshold_list, constraint_confidence_list=constraint_confidence_list,
             n_init=10, n_repeat=n_repeat, train_times=train_times, regularize=False, low_dim=True,
             spectrum_norm=False, retrain_interval=1, n_iter=n_iter, filter_interval=1, acq="ci", 
             ci_intersection=True, verbose=True, lr=1e-4, name="test", return_result=True, retrain_nn=True,
             plot_result=True, save_result=True, save_path='./res', fix_seed=True,  pretrained=False, ae_loc=None, 
             _minimum_pick = 10, _delta = 0.2, beta=0, filter_beta=0.5, exact_gp=False, constrain_noise=False, local_model=False)
 
-regret = cbo(x_tensor, y_tensor, c_tensor, constraint_threshold=0, constraint_confidence=.5,
+regret = cbo_multi(x_tensor, y_tensor, c_tensor_list, constraint_threshold_list=constraint_threshold_list, constraint_confidence_list=constraint_confidence_list,
             n_init=10, n_repeat=n_repeat, train_times=train_times, regularize=False, low_dim=True,
             spectrum_norm=False, retrain_interval=1, n_iter=n_iter, filter_interval=1, acq="ci", 
             ci_intersection=True, verbose=True, lr=1e-4, name="test", return_result=True, retrain_nn=True,