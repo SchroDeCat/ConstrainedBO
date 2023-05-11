83ece3827cd5ee67701b550759d19839562ba17c
Fengxue Zhang <zfxrex@live.cn>
2023-05-11 15:07:51 -0500

    implement cbo_m class, remove phase 2

--- src/opt/dkbo_ae_constrained.py
+++ src/opt/dkbo_ae_constrained.py
@@ -295,7 +295,7 @@ class DK_BO_AE_C_M():
     """
     Initialize the network with auto-encoder for constrained setting, support list of constraints
     """
-    def __init__(self, x_tensor, y_tensor, c_tensor, roi_filter, c_uci_filter, c_threshold,
+    def __init__(self, x_tensor, y_tensor, c_tensor_list, roi_filter, c_uci_filter_list, c_threshold_list,
                     n_init:int=10, lr=1e-6, train_iter:int=10, regularize=True, spectrum_norm=False,
                     dynamic_weight=False, verbose=False, max=None, robust_scaling=True, pretrained_nn=None, low_dim=True,
                     record_loss=False, retrain_nn=True, exact_gp=False, noise_constraint=None, **kwargs):
@@ -308,6 +308,7 @@ class DK_BO_AE_C_M():
         self.regularize = regularize
         self.lr = lr
         self.low_dim = low_dim
+        self.c_num = len(c_tensor_list)
         self.verbose = verbose
         self.n_init = n_init
         self.n_neighbors = min(self.n_init, 10)
@@ -315,7 +316,7 @@ class DK_BO_AE_C_M():
         self.dynamic_weight = dynamic_weight
         self.x_tensor = torch.from_numpy(x_tensor).float()
         self.y_tensor = y_tensor.float()
-        self.c_tensor = c_tensor.float()
+        self.c_tensor_list = c_tensor_list.float()
         self.data_size = self.x_tensor.size(0)
         self.train_iter = train_iter
         self.retrain_nn = retrain_nn
@@ -324,7 +325,7 @@ class DK_BO_AE_C_M():
 
         self.init_x = kwargs.get("init_x", self.x_tensor[:n_init])        
         self.init_y = kwargs.get("init_y", self.y_tensor[:n_init])
-        self.init_c = kwargs.get("init_c", self.c_tensor[:n_init])
+        self.init_c_list = kwargs.get("init_c_list", [c_tensor[:n_init] for c_tensor in self.c_tensor_list])
         assert self.init_x.size(0) == self.init_c.size(0)
         if "init_x" in kwargs:
             self.init_x = torch.from_numpy(self.scaler.transform(self.init_x)).float()
@@ -334,10 +335,12 @@ class DK_BO_AE_C_M():
         self.observed = np.zeros(self.x_tensor.size(0)).astype("int")
         self.pretrained_nn = pretrained_nn
         self.roi_filter = roi_filter
-        self.c_uci_filter = c_uci_filter
-        self.c_threshold = c_threshold
+        self.c_uci_filter_list = c_uci_filter_list
+        self.c_threshold_list = c_threshold_list
+
+        # load input model / train the ROI model
         f_model = kwargs.get("f_model", None)
-        c_model = kwargs.get("c_model", None)
+        c_model_list = kwargs.get("c_model_list", None)
 
         if f_model is None:
             self.f_model = DKL(self.init_x, self.init_y.squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim, 
@@ -346,12 +349,12 @@ class DK_BO_AE_C_M():
         else:
             self.f_model = f_model
         
-        if c_model is None:
-            self.c_model = DKL(self.init_x, self.init_c.squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim, 
-                            pretrained_nn=self.pretrained_nn, retrain_nn=retrain_nn, spectrum_norm=spectrum_norm, exact_gp=exact_gp, 
-                            noise_constraint = self.noise_constraint)
+        if c_model_list is None:
+            self.c_model_list = [DKL(self.init_x, self.init_c_list[c_idx].squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim,
+                                    pectrum_norm=spectrum_norm, exact_gp=exact_gp, pretrained_nn=self.pretrained_nn, retrain_nn=retrain_nn,
+                                    noise_constraint=self.noise_constraint,) for c_idx in range(self.c_num)]
         else:
-            self.c_model = c_model
+            self.c_model_list = c_model_list
         
         self.record_loss = record_loss
 
@@ -367,16 +370,19 @@ class DK_BO_AE_C_M():
     def train(self,):
         if self.regularize:
             self.f_model.train_model_kneighbor_collision(self.n_neighbors, Lambda=self.Lambda, dynamic_weight=self.dynamic_weight, return_record=False, verbose=self.verbose)
-            self.c_model.train_model_kneighbor_collision(self.n_neighbors, Lambda=self.Lambda, dynamic_weight=self.dynamic_weight, return_record=False, verbose=self.verbose)
+            for c_idx in range(self.c_num):
+                self.c_model_list[c_idx].train_model_kneighbor_collision(self.n_neighbors, Lambda=self.Lambda, dynamic_weight=self.dynamic_weight, return_record=False, verbose=self.verbose)
         else:
             
             if self.record_loss:
                 self._f_pure_dkl.train_model(record_mae=True)
                 self.f_model.train_model(record_mae=True)
-                self.c_model.train_model(record_mae=True)
+                for c_idx in range(self.c_num):
+                    self.c_model_list[c_idx].train_model(record_mae=True)
             else:
                 self.f_model.train_model(verbose=False)
-                self.c_model.train_model(verbose=False)
+                for c_idx in range(self.c_num):
+                    self.c_model_list[c_idx].train_model(verbose=False)
 
         
 
@@ -394,8 +400,8 @@ class DK_BO_AE_C_M():
         ci_intersection = kwargs.get("ci_intersection", False)
         f_max_test_x_lcb = kwargs.get("f_max_test_x_lcb", None)
         f_min_test_x_ucb = kwargs.get("f_min_test_x_ucb", None)
-        c_max_test_x_lcb = kwargs.get("c_max_test_x_lcb", None)
-        c_min_test_x_ucb = kwargs.get("c_min_test_x_ucb", None)
+        c_max_test_x_lcb_list = kwargs.get("c_max_test_x_lcb_list", None)
+        c_min_test_x_ucb_list = kwargs.get("c_min_test_x_ucb_list", None)
 
         beta = kwargs.get("beta", 1)
         _delta = kwargs.get("delta", .2)
@@ -404,39 +410,52 @@ class DK_BO_AE_C_M():
         _candidate_idx_list = np.zeros(n_iter)
         ### optimization loop
         for i in iterator:
+            _candidate_idx_c_list = [None for i in range(self.c_num)]
             if real_beta:
                 beta = (2 * np.log((self.x_tensor.size(0) * (np.pi * (self.init_x.size(0) + 1)) ** 2) /(6 * _delta))) ** 0.5
+            # acq values
             if ci_intersection:
                 assert not( f_max_test_x_lcb is None or f_min_test_x_ucb is None)
                 _candidate_idx_f = self.f_model.intersect_CI_next_point(self.x_tensor[self.roi_filter], 
                                                                         max_test_x_lcb=f_max_test_x_lcb[self.roi_filter], 
                                                                         min_test_x_ucb=f_min_test_x_ucb[self.roi_filter], 
                                                                         acq=acq, beta=beta, return_idx=True)
-                if sum(self.c_uci_filter) > 0:
-                    _candidate_idx_c = self.c_model.intersect_CI_next_point(self.x_tensor[self.c_uci_filter], 
-                                                            max_test_x_lcb=c_max_test_x_lcb[self.c_uci_filter], 
-                                                            min_test_x_ucb=c_min_test_x_ucb[self.c_uci_filter], 
-                                                            acq=acq, beta=beta, return_idx=True)
+
+                for c_idx, (c_uci_filter, c_max_test_x_lcb, c_min_test_x_ucb) in enumerate(zip(self.c_uci_filter_list, c_max_test_x_lcb_list, c_min_test_x_ucb_list)):
+                    if sum(c_uci_filter) > 0:
+                        _candidate_idx_c = self.c_model_list[c_idx].intersect_CI_next_point(self.x_tensor[c_uci_filter], 
+                                                                max_test_x_lcb=c_max_test_x_lcb[c_uci_filter], 
+                                                                min_test_x_ucb=c_min_test_x_ucb[c_uci_filter], 
+                                                                acq=acq, beta=beta, return_idx=True)
+                        _candidate_idx_c_list[c_idx] = _candidate_idx_c
             else:
                 _candidate_idx_f = self.f_model.next_point(self.x_tensor[self.roi_filter], acq, "love", return_idx=True, beta=beta,)
-                if sum(self.c_uci_filter) > 0:
-                    _candidate_idx_c = self.c_model.next_point(self.x_tensor[self.c_uci_filter], acq, "love", return_idx=True, beta=beta,)
+                for c_idx, c_uci_filter in enumerate(self.c_uci_filter_list):
+                    if sum(c_uci_filter) > 0:
+                        _candidate_idx_c = self.c_model.next_point(self.x_tensor[c_uci_filter], acq, "love", return_idx=True, beta=beta,)
             
+            # locate max acq
             _f_acq = self.f_model.acq_val[_candidate_idx_f]
-            if sum(self.c_uci_filter) > 0:
-                _c_acq = self.c_model.acq_val[_candidate_idx_c]
-        
-            if sum(self.c_uci_filter) > 0 and _c_acq > _f_acq:
-                candidate_idx = util_array[self.c_uci_filter][_candidate_idx_c]
+            _c_acq_list = torch.tensor([float('-inf') for _ in range(self.c_num)])
+            for c_idx, c_uci_filter in enumerate(self.c_uci_filter_list):
+                if sum(c_uci_filter) > 0:
+                    _c_acq_list[c_idx] = self.c_model_list[c_idx].acq_val[_candidate_idx_c_list[c_idx]]
+            _c_acq_list_max = torch.max(_c_acq_list, dim=0)
+
+            if _c_acq_list_max.values > _f_acq:
+                assert _c_acq_list_max.values > float("-inf")
+                _c_idx = _c_acq_list_max.indices
+                candidate_idx = util_array[self.c_uci_filter_list[_c_idx]][_candidate_idx_c_list[_c_idx]]
             else:
                 candidate_idx = util_array[self.roi_filter][_candidate_idx_f]
 
-
+            # update obs
             _candidate_idx_list[i] = candidate_idx
             self.init_x = torch.cat([self.init_x, self.x_tensor[candidate_idx].reshape(1,-1)], dim=0)
             self.init_y = torch.cat([self.init_y, self.y_tensor[candidate_idx].reshape(1,-1)])
-            self.init_c = torch.cat([self.init_c, self.c_tensor[candidate_idx].reshape(1,-1)])
-            assert self.init_x.size(0) == self.init_c.size(0)
+            for c_idx in range(self.c_num):
+                self.init_c_list[c_idx] = torch.cat([self.init_c_list[c_idx], self.c_tensor_list[c_idx][candidate_idx].reshape(1,-1)])
+                assert self.init_x.size(0) == self.init_c_list[c_idx].size(0)
             assert self.init_x.size(0) == self.init_y.size(0)
             self.observed[candidate_idx] = 1
 
@@ -445,14 +464,14 @@ class DK_BO_AE_C_M():
                 self._f_state_dict_record = self.f_model.feature_extractor.state_dict()
                 self._f_output_scale_record = self.f_model.model.covar_module.base_kernel.outputscale
                 self._f_length_scale_record = self.f_model.model.covar_module.base_kernel.base_kernel.lengthscale
-                self._c_state_dict_record = self.c_model.feature_extractor.state_dict()
-                self._c_output_scale_record = self.c_model.model.covar_module.base_kernel.outputscale
-                self._c_length_scale_record = self.c_model.model.covar_module.base_kernel.base_kernel.lengthscale
+                self._c_state_dict_record_list = [self.c_model_list[c_idx].feature_extractor.state_dict() for c_idx in range(self.c_num)]
+                self._c_output_scale_record_list = [self.c_model_list[c_idx].model.covar_module.base_kernel.outputscale for c_idx in range(self.c_num)]
+                self._c_length_scale_record_list = [self.c_model_list[c_idx].model.covar_module.base_kernel.base_kernel.lengthscale for c_idx in range(self.c_num)]
 
             self.f_model = DKL(self.init_x, self.init_y.squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim, pretrained_nn=self.pretrained_nn, retrain_nn=self.retrain_nn,
                                  spectrum_norm=self.spectrum_norm, exact_gp=self.exact, noise_constraint=self.noise_constraint)
-            self.c_model = DKL(self.init_x, self.init_c.squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim, pretrained_nn=self.pretrained_nn, retrain_nn=self.retrain_nn,
-                                 spectrum_norm=self.spectrum_norm, exact_gp=self.exact, noise_constraint=self.noise_constraint)
+            self.c_model_list = [DKL(self.init_x, self.init_c_list[c_idx].squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim, pretrained_nn=self.pretrained_nn, retrain_nn=self.retrain_nn,
+                                 spectrum_norm=self.spectrum_norm, exact_gp=self.exact, noise_constraint=self.noise_constraint) for c_idx in range(self.c_num)]
             
             if self.record_loss:
                 self._f_pure_dkl = DKL(self.init_x, self.init_y.squeeze(), n_iter=self.train_iter, low_dim=self.low_dim, pretrained_nn=None, lr=self.lr, spectrum_norm=self.spectrum_norm, exact_gp=self.exact)
@@ -461,10 +480,10 @@ class DK_BO_AE_C_M():
                 self.f_model.feature_extractor.load_state_dict(self._f_state_dict_record, strict=False)
                 self.f_model.model.covar_module.base_kernel.outputscale = self._f_output_scale_record
                 self.f_model.model.covar_module.base_kernel.base_kernel.lengthscale = self._f_length_scale_record
-                
-                self.c_model.feature_extractor.load_state_dict(self._c_state_dict_record, strict=False)
-                self.c_model.model.covar_module.base_kernel.outputscale = self._c_output_scale_record
-                self.c_model.model.covar_module.base_kernel.base_kernel.lengthscale = self._c_length_scale_record
+                for c_idx in range(self.c_num):
+                    self.c_model_list[c_idx].feature_extractor.load_state_dict(self._c_state_dict_record_list[c_idx], strict=False)
+                    self.c_model_list[c_idx].model.covar_module.base_kernel.outputscale = self._c_output_scale_record_list[c_idx]
+                    self.c_model_list[c_idx].model.covar_module.base_kernel.base_kernel.lengthscale = self._c_length_scale_record_list[c_idx]
             else:
                 self.train()
             if self.record_loss:
@@ -476,73 +495,6 @@ class DK_BO_AE_C_M():
             if sum(feasible_obs_filter) > 0:
                 _max_reward = torch.max(self.init_y[feasible_obs_filter])
                 self.regret[i] = self.maximum - _max_reward
-            else:
-                self.regret[i] = self.max_regret
-
-            if self.regret[i] < 1e-10 and early_stop:
-                break
-            if if_tqdm:
-                iterator.set_postfix({"regret":self.regret[i], "Internal_beta": beta})
-
-
-    def query_cons(self, n_iter:int=10, acq="ucb", retrain_interval:int=1, **kwargs):
-        '''
-        Second Stage: Optimize C only simultaneously
-        '''
-        self.regret = np.zeros(n_iter)
-        if_tqdm = kwargs.get("if_tqdm", False)
-        early_stop = kwargs.get("early_stop", True)
-        iterator = tqdm.tqdm(range(n_iter)) if if_tqdm else range(n_iter)
-        util_array = np.arange(self.data_size)
-        ci_intersection = kwargs.get("ci_intersection", False)
-        c_max_test_x_lcb = kwargs.get("c_max_test_x_lcb", None)
-        c_min_test_x_ucb = kwargs.get("c_min_test_x_ucb", None)
-
-        beta = kwargs.get("beta", 1)
-        _delta = kwargs.get("delta", .2)
-
-        real_beta = beta <= 0 # if using analytic beta
-        _candidate_idx_list = np.zeros(n_iter)
-        ### optimization loop
-        for i in iterator:
-            if real_beta:
-                beta = (2 * np.log((self.x_tensor.size(0) * (np.pi * (self.init_x.size(0) + 1)) ** 2) /(6 * _delta))) ** 0.5
-            if ci_intersection:
-                _candidate_idx_c = self.c_model.intersect_CI_next_point(self.x_tensor[self.c_uci_filter], 
-                                                        max_test_x_lcb=c_max_test_x_lcb[self.c_uci_filter], 
-                                                        min_test_x_ucb=c_min_test_x_ucb[self.c_uci_filter], 
-                                                        acq=acq, beta=beta, return_idx=True)
-            else:
-                _candidate_idx_c = self.c_model.next_point(self.x_tensor[self.c_uci_filter], acq, "love", return_idx=True, beta=beta,)
-
-            candidate_idx = util_array[self.c_uci_filter][_candidate_idx_c]
-
-            _candidate_idx_list[i] = candidate_idx
-            self.init_x = torch.cat([self.init_x, self.x_tensor[candidate_idx].reshape(1,-1)], dim=0)
-            self.init_y = torch.cat([self.init_y, self.y_tensor[candidate_idx].reshape(1,-1)])
-            self.init_c = torch.cat([self.init_c, self.c_tensor[candidate_idx].reshape(1,-1)])
-            self.observed[candidate_idx] = 1
-
-            # retrain
-            if i % retrain_interval != 0 and self.low_dim: # allow skipping retrain in low-dim setting
-                self._c_state_dict_record = self.c_model.feature_extractor.state_dict()
-                self._c_output_scale_record = self.c_model.model.covar_module.base_kernel.outputscale
-                self._c_length_scale_record = self.c_model.model.covar_module.base_kernel.base_kernel.lengthscale
-
-            self.c_model = DKL(self.init_x, self.init_c.squeeze(), n_iter=self.train_iter, lr= self.lr, low_dim=self.low_dim, pretrained_nn=self.pretrained_nn, retrain_nn=self.retrain_nn,
-                                 spectrum_norm=self.spectrum_norm, exact_gp=self.exact, noise_constraint=self.noise_constraint)
-               
-            if i % retrain_interval != 0 and self.low_dim:
-                self.c_model.feature_extractor.load_state_dict(self._c_state_dict_record, strict=False)
-                self.c_model.model.covar_module.base_kernel.outputscale = self._c_output_scale_record
-                self.c_model.model.covar_module.base_kernel.base_kernel.lengthscale = self._c_length_scale_record
-            else:
-                self.train()
-
-            # regret & early stop
-            feasible_obs_filter = self.init_c > self.c_threshold
-            if sum(feasible_obs_filter) > 0:
-                self.regret[i] = self.maximum - torch.max(self.init_y[feasible_obs_filter])
             else:
                 self.regret[i] = self.max_regret
 
