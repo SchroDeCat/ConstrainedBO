a8a45968dad73632225e0faec6092d440a369019
Alex Zhang <zfxrex@live.cn>
2023-09-03 00:27:45 -0500

    fix vessel performance by reducing pts

--- test_general.py
+++ test_general.py
@@ -126,7 +126,8 @@ def experiment(exp:str='rastrigin_1d', method:str='qei', n_repeat:int=2, train_t
         beta = 20
 
     elif exp == "vessel_4d_3c":
-        cbo_factory = Constrained_Data_Factory(num_pts=40000)
+        # cbo_factory = Constrained_Data_Factory(num_pts=40000)
+        cbo_factory = Constrained_Data_Factory(num_pts=10000)
         scbo = 'scbo' in method
         if scbo:
             x_tensor, y_func, c_func_list = cbo_factory.RE2_4D_3C(scbo_format=scbo)
@@ -288,33 +289,42 @@ if __name__ == "__main__":
     
     # experiment(n_init=10, method='cbo')
     # experiment(exp='water_converter_32d_neg', n_init=10, n_repeat=n_repeat, n_iter=n_iter, method='cbo', constrain_noise=True)
-
+    # originally train_iter = 50, now 5
     # experiment(exp='water_converter_32d_neg_3c', n_init=n_init3, n_repeat=n_repeat, n_iter=n_iter, method='cbo',)
     # experiment(exp='water_converter_32d_neg_3c', n_init=n_init3, n_repeat=n_repeat, n_iter=n_iter, method='scbo',)
     # experiment(exp='water_converter_32d_neg_3c', n_init=n_init3, n_repeat=n_repeat, n_iter=n_iter, method='qei', )
     # experiment(exp='water_converter_32d_neg_3c', n_init=n_init3, n_repeat=n_repeat, n_iter=n_iter, method='cmes-ibo', )
 
-    for method in METHODs:
-        # if method in ['cbo', 'cmes-ibo']:
-        if method in ['ts', 'random']:
-            continue
-        # experiment(exp='water_converter_32d_neg_3c', n_init=20, n_repeat=10, n_iter=100, method=method, constrain_noise=True)
-        experiment(exp="vessel_4D_3C", n_init=2, n_iter=200, n_repeat=15, method=method)
+    # for method in METHODs:
+    #     # if method in ['cbo', 'cmes-ibo']:
+    #     if method in ['ts', 'random']:
+    #         continue
 
-    for method in METHODs:
-        # if method in ['cbo', 'qei']:
-        #     continue
-        if method in ['ts', 'random']:
-            continue
-        # experiment(exp='water_converter_32d_neg_3c', n_init=20, n_repeat=10, n_iter=100, method=method, constrain_noise=True)
-        experiment(exp="car_cab_7d_8c", n_init=5, n_iter=200, n_repeat=15, method=method)
+    #     # experiment(exp='water_converter_32d_neg_3c', n_init=n_init3, n_repeat=2, n_iter=2, method=method, )
+    #     experiment(exp='water_converter_32d_neg_3c', n_init=n_init3, n_repeat=n_repeat, n_iter=200, method=method, )
 
 
-    for method in METHODs:
-        # if method in ['cbo', 'qei']:
-        #     continue
-        if method in ['ts', 'random']:
-            continue
-        # experiment(exp='water_converter_32d_neg_3c', n_init=20, n_repeat=10, n_iter=100, method=method, constrain_noise=True)
-        experiment(exp="spring_3d_6c", n_init=10, n_iter=200, n_repeat=15, method=method)
+    experiment(exp="vessel_4D_3C", n_init=2, n_iter=200, n_repeat=15, method='cmes-ibo')
+    # for method in METHODs:
+    #     # if method in ['cbo', 'cmes-ibo']:
+    #     if method in ['ts', 'random']:
+    #         continue
+    #     experiment(exp="vessel_4D_3C", n_init=2, n_iter=200, n_repeat=15, method=method)
+
+    # for method in METHODs:
+    #     # if method in ['cbo', 'qei']:
+    #     #     continue
+    #     if method in ['ts', 'random']:
+    #         continue
+    #     # experiment(exp='water_converter_32d_neg_3c', n_init=20, n_repeat=10, n_iter=100, method=method, constrain_noise=True)
+    #     experiment(exp="car_cab_7d_8c", n_init=5, n_iter=200, n_repeat=15, method=method)
+
+
+    # for method in METHODs:
+    #     # if method in ['cbo', 'qei']:
+    #     #     continue
+    #     if method in ['ts', 'random']:
+    #         continue
+    #     # experiment(exp='water_converter_32d_neg_3c', n_init=20, n_repeat=10, n_iter=100, method=method, constrain_noise=True)
+    #     experiment(exp="spring_3d_6c", n_init=10, n_iter=200, n_repeat=15, method=method)
     # experiment(exp='spring_3d_6c', n_init=10, n_repeat=10, n_iter=n_iter, method='scbo')
