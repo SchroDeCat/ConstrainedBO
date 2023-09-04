5af21a176d4073da6f7a0c43e1c573994a947f8f
Fengxue Zhang <zfxrex@live.cn>
2023-09-04 16:01:51 -0500

    update with new setup

--- visualize_res.py
+++ visualize_res.py
@@ -56,12 +56,12 @@ ax.set_title("Ackley-5D-2C")
 
 # Wave-Energy_Converter-36D
 RES_num = {}
-RES_num["CBO"] = np.load(f"{CBO_DIR}OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-B20.00-FB20.00-RI1--none-ci-R15-P2-T50_I1_L4-TI50-USexact.npy")
-RES_num["CMES-IBO"] = np.load(f"{BASELINE_DIR}cmes-ibo/OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-RI1--none-cmes-ibo-R15-P2-T50_I1_L4-TI50-USexact.npy")
-RES_num["cEI"] = np.load(f"{BASELINE_DIR}qei/OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-RI1--none-qei-R15-P2-T50_I1_L4-TI50-USexact.npy")
-RES_num["SCBO"] = np.load(f"{SCBO_DIR}OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-RI1--none-scbo-R15-P2-T200_I1_L4-TI50-USexact.npy")
+RES_num["CBO"] = np.load(f"{CBO_DIR}OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-B20.00-FB20.00-RI1--none-ci-R15-P2-T200_I1_L4-TI5-USexact.npy")
+RES_num["CMES-IBO"] = np.load(f"{BASELINE_DIR}cmes-ibo/OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-RI1--none-cmes-ibo-R15-P2-T200_I1_L4-TI5-USexact.npy")
+RES_num["cEI"] = np.load(f"{BASELINE_DIR}qei/OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-RI1--none-qei-R15-P2-T200_I1_L4-TI5-USexact.npy")
+RES_num["SCBO"] = np.load(f"{SCBO_DIR}OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-RI1--none-scbo-R15-P2-T200_I1_L4-TI5-USexact.npy")
 ax = plt.subplot(2,3,3)
-visualize_regret(ax=ax, RES=RES_num, fontsize=fontsize, n_repeat=15, n_iter=50)
+visualize_regret(ax=ax, RES=RES_num, fontsize=fontsize, n_repeat=15, n_iter=200)
 ax.set_title("Converter-36D-3C")
 
 # vessel 
@@ -100,5 +100,5 @@ ax.set_title("spring-3D-6C")
 # plt.tight_layout()
 fig.legend(handles, labels, loc='upper center', ncol=len(labels))
 plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=None)
-plt.savefig("test.png")
+plt.savefig("simple_regret.png")
 # plt.show()
