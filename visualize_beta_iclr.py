6382131d47cda672b24b26885c4afec84c56fbe0
Fengxue Zhang <zfxrex@live.cn>
2023-11-17 11:21:39 -0600

    improve caption

--- visualize_beta_iclr.py
+++ visualize_beta_iclr.py
@@ -19,7 +19,8 @@ def visualize_regret(
     ax: plt.Axes, RES: dict, fontsize: int = 14, n_repeat: int = 15, n_iter: int = 100
 ) -> None:
     sqrt_n = np.sqrt(n_repeat)
-    init_regret = RES[f"Beta {BETA[0]}"][:, 0].mean(axis=0)
+    # init_regret = RES[f"Beta {BETA[0]}"][:, 0].mean(axis=0)
+    init_regret = RES[f"Theoretical Beta"][:, 0].mean(axis=0)
     for method in RES_num.keys():
         CI = 1.96
         coef = CI / sqrt_n
@@ -45,7 +46,8 @@ for beta in BETA:
         _pre = f"OL-Regret-Figure_RASTRIGIN_1D-InterP-B{beta:.2f}"
         _ext = 'npy'
         if _file.startswith(_pre) and _file.endswith(_ext) and ('B0.00-FB1.00' not in _file):
-            RES_num[f"Beta {beta}"] = np.load(
+            _key = f"Beta {beta}" if beta > 0 else f"Theoretical Beta"
+            RES_num[_key] = np.load(
                 f"{CBO_DIR}/{_file}"
             )
 
@@ -64,7 +66,8 @@ for beta in BETA:
         _pre = f"OL-Regret-Figure_ACKLEY_5D-noise_c-InterP-B{beta:.2f}"
         _ext = 'npy'
         if _file.startswith(_pre) and _file.endswith(_ext) and ('B0.00-FB4.00' not in _file):
-            RES_num[f"Beta {beta}"] = np.load(
+            _key = f"Beta {beta}" if beta > 0 else f"Theoretical Beta"
+            RES_num[_key] = np.load(
                 f"{CBO_DIR}/{_file}"
             )
 
@@ -81,7 +84,8 @@ for beta in BETA:
         _pre = f"OL-Regret-Figure_WATER_CONVERTER_32D_NEG_3C-InterP-B{beta:.2f}"
         _ext = 'npy'
         if _file.startswith(_pre) and _file.endswith(_ext) and ('B0.00-FB20.00' not in _file):
-            RES_num[f"Beta {beta}"] = np.load(
+            _key = f"Beta {beta}" if beta > 0 else f"Theoretical Beta"
+            RES_num[_key] = np.load(
                 f"{CBO_DIR}/{_file}"
             )
 
